ab139d298f7254dd649d4050a889008d9ece9398
priyarajesh7696 <rameshpriyait@gmail.com>
2023-12-03 22:32:13 +0530

    Task completed

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 
 function add() {
   var x = document.getElementById("form");
-  var tr = document.getElementById("data");
-
+  var tbody = document.getElementById("tbody");
+  var tr = document.createElement("tr");
   for ( var i = 0; i < x.length; i++) {
+    
     var td = document.createElement("td");
+    console.log(x.elements[i].value);
     td.innerHTML += x.elements[i].value;
     tr.appendChild(td);
+    
   }
+  tbody.appendChild(tr);
+  x.reset();
 }
+
+
